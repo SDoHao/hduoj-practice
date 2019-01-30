3c7779c5526b6a31cdc0d8c7b698b74aa4471bc6
SDoHao <19951122v2@163.com>
2019-01-30 16:04:05 +0800

    git add 2007.c!

--- 2007.c
+++ 2007.c
@@ -4,6 +4,10 @@ int main()
     long int x,y;
     while(scanf("%ld %ld",&x,&y)!=EOF)
     {
+        if(x>y//!!!
+        {
+            long int tmp=x;x=y;y=tmp;
+        }
         long int i=x,even_pro = 0,odd_pro = 0;
         if(i%2==0)
         {
