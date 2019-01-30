73cc35270eea729d0360adcc4835a51882fa2c44
SDoHao <19951122v2@163.com>
2019-01-30 16:26:22 +0800

    AC 2008

--- 2008.c
+++ 2008.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 int main()
 {
+    int n;
+    double m;
+    while(scanf("%d",&n)!=EOF&&n!=0)
+    {
+        int i = 0,positives =0,negatives =0,zeros = 0;
+        while(i<n)
+        {
+            scanf("%lf",&m);
+            if(m==0)
+            {
+                zeros++;
+            }
+            else if(m<0)
+            {
+                negatives++;
+            }
+            else
+            {
+                positives++;
+            }
+            i++;
+        }
+        printf("%d %d %d\n",negatives,zeros,positives);
+    }
 
     return 0;
 }
