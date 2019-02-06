53164247f22b2b11b7e315861dfbe6d21690f4ff
SDoHao <19951122v2@163.com>
2019-02-06 20:53:43 +0800

    2033 new

---
+++ 2033.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int i=0,n;
+    scanf("%d",&n);
+    while(i<n)
+     {
+        int AH,AM,AS,BH,BM,BS;
+        scanf("%d %d %d %d %d %d",&AH,&AM,&AS,&BH,&BM,&BS);
+        AS+=BS;
+        while(AS>59)
+        {
+            AM++;
+            AS-=60;
+        }
+
+        AM+=BM;
+        while(AM>59)
+        {
+            AH++;
+            AM-=60;
+        }
+        i++;
+        AH+=BH;
+        printf("%d %d %d\n",AH,AM,AS);
+     }
+    return 0;
+}
